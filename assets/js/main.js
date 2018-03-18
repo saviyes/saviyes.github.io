6e3506b88250aaff735e3e51d7177bf0c824bbb8
saviyes <37488413+saviyes@users.noreply.github.com>
2018-03-18 13:53:25 +0000

    top menu js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,37 @@
 	templated.co @templatedco
 	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
 */
+
+[].slice.call(document.querySelectorAll('.dropdown .nav-link')).forEach(function(el){
+    el.addEventListener('click', onClick, false);
+});
+
+function onClick(e){
+    e.preventDefault();
+    var el = this.parentNode;
+    el.classList.contains('show-submenu') ? hideSubMenu(el) : showSubMenu(el);
+}
+
+function showSubMenu(el){
+    el.classList.add('show-submenu');
+    document.addEventListener('click', function onDocClick(e){
+        e.preventDefault();
+        if(el.contains(e.target)){
+            return;
+        }
+        document.removeEventListener('click', onDocClick);
+        hideSubMenu(el);
+    });
+}
+
+function hideSubMenu(el){
+    el.classList.remove('show-submenu');
+}
+
+/* fin code top menu */
+
+
+
 (function($) {
 
 	var	$window = $(window),
